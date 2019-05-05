9991b2014f49b5d009873731fed9eb3b2cef8065
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-05 00:05:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!":
+  if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase.upcase:
+  elsif phrase.upcase
     "NO, NOT SINCE 1938!"
-  elsif phrase.downcase:
+  elsif phrase.downcase
     "HUH?! SPEAK UP, SONNY!"
   end
 end
